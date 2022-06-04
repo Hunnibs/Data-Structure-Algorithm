592d770cbe375ac18d0ec443c4f3cd32820c82c6
Hunnibs <cqqudgjs@hufs.ac.kr>
2022-06-04 21:19:24 +0900

    주석 추가

--- CompositeFunction.py
+++ CompositeFunction.py
@@ -1,4 +1,8 @@
-def CompositeFunction(x, k):
+'''
+트리를 리스트 형식으로 구현 F(x) = value(0 <= x <= n-1)에서 x를 인덱스 값으로 해당 리스트에 value값을 저장
+각 질의 당 시간 복잡도는 O(k)만큼 걸린다. 
+'''
+def CompositeFunction(x, k):  # 계속해서 타고 내려가는 방식
 	value = x
 	for i in range(k):
 		value = Func[value]
