cf55c4f96d8cbd4fa595cc9e09b4de4995372544
Hunnibs <cqqudgjs@hufs.ac.kr>
2022-11-16 14:40:41 +0900

    Silver

--- BAEKJOON/14620.py
+++ BAEKJOON/14620.py
@@ -1,25 +1,47 @@
-def sol(i, j):
+def sol(sum, cnt):
+    global result
+    
     if cnt == 3:
+        result.append(sum)
         return
-
-    if cost[i][j] == 0:
-        result += cost[i][j]
-
+    
+    for i in range(1, n-1):
+        for j in range(1, n-1):
+            if check[i][j] or check[i-1][j] or check [i+1][j] or check[i][j-1] or check[i][j+1]:  # 심을 수 없는 경우
+                continue
+            else:  # 심었을 경우
+                sum += tile[i][j] + tile[i-1][j] + tile[i+1][j] + tile[i][j-1] + tile[i][j+1]
+                check[i][j] = 1
+                check[i][j+1] = 1
+                check[i][j-1] = 1
+                check[i-1][j] = 1
+                check[i+1][j] = 1
+                cnt += 1
+                
+                sol(sum, cnt)
+                
+                sum -= (tile[i][j] + tile[i-1][j] + tile[i+1][j] + tile[i][j-1] + tile[i][j+1])
+                check[i][j] = 0
+                check[i][j+1] = 0
+                check[i][j-1] = 0
+                check[i-1][j] = 0
+                check[i+1][j] = 0
+                cnt -= 1
+        
 # input
 n = int(input())
 tile = [list(map(int, input().split())) for _ in range(n)]
 
-cost = []
 check = []
-for i in range(n-2):
-    cost.append([])
+for i in range(n):
     check.append([])
 
-for i in range(n-2):
-    for j in range(n-2):
-        cost[i].append(tile[i+1][j+1] + tile[i+1][j] + tile[i+1][j+2] + tile[i][j+1] + tile[i+2][j+1])
+for i in range(n):
+    for j in range(n):
         check[i].append(0)
 
 cnt = 0
-result = 0
-sol(i, j)
+sum = 0
+result = []
+sol(sum, cnt)
+print(min(result))
