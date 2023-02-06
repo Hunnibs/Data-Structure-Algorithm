b9a9fc142ee5f4eddd5817a35fd6b042a6ddf6e1
Hunnibs <cqqudgjs@hufs.ac.kr>
2023-02-06 16:06:50 +0900

    BackTracking

--- BAEKJOON/15649.py
+++ BAEKJOON/15649.py
@@ -1,25 +1,31 @@
 import copy
-def sol(num, visited):
-    tmp = check(num, visited)
-    if :
-        for i in range(0,)
+
+def sol(num, visited, result):
+    tmp_arr, tmp_result = check(num, visited, result)
+
+    if len(tmp_result) == M:
+        for num in tmp_result:
+            print(num, end=' ')
+        print()
         return
 
     for i in range(N):
-        if tmp[i] == 0:
-            sol(i+1, tmp)
+        if tmp_arr[i] == False:
+            sol(i+1, tmp_arr, tmp_result)
 
-def check(num, visited):
+def check(num, visited, result):
     tmp = copy.deepcopy(visited)
-    tmp[num-1] = num
+    result = copy.deepcopy(result)
 
-    return tmp
+    tmp[num-1] = True
+    result.append(num)
+
+    return tmp, result
 
 # input
 N, M = map(int, input().split())
 
-visited = [0] * N
-result = [0] * M
-
+result = []
+visited = [False] * N
 for i in range(N):
-    sol(i+1, visited)
+    sol(i+1, visited, result)
