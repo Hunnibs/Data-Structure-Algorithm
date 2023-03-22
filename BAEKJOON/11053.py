ca1f397822b2a265b4305e78f0e2fa1d12b9b257
Hunnibs <cqqudgjs@hufs.ac.kr>
2023-03-22 11:23:27 +0900

    Siver_DP

---
+++ BAEKJOON/11053.py
@@ -0,0 +1,18 @@
+import sys
+
+# input
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [0 for _ in range(n)]
+
+dp[0] = 1
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+    if dp[i] == 0:
+        dp[i] = 1
+
+print(max(dp))
